804a7bbae1b3aa33cc0c1458c9002e85dca10bae
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-09 00:30:45 -0700

    Add project files.

---
+++ TraversalInSpiralOrder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TraversalInSpiralOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(1);
+            tree.left = new Node(2);
+            tree.left.left = new Node(4);
+            tree.left.left.left = new Node(8);
+            tree.left.right = new Node(5);
+            tree.left.right.left = new Node(9);
+            tree.right = new Node(3);
+            tree.right.left = new Node(6);
+            tree.right.right = new Node(7);
+            tree.right.right.right = new Node(10);
+
+            TraversalInSpiral TIS = new TraversalInSpiral();
+            TIS.InSpiralOrder(tree);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TraversalInSpiralOrder/TraversalInSpiral.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraversalInSpiralOrder
+{
+    class TraversalInSpiral
+    {
+       public void InSpiralOrder(Node root)
+        {
+            Stack<Node> S1 = new Stack<Node>();
+            Stack<Node> S2 = new Stack<Node>();
+
+            S1.Push(root);
+            
+            while (S1.Count != 0 || S2.Count != 0)
+            {
+                Console.WriteLine();
+                while (S1.Count != 0)
+                {
+                    root = S1.Pop();
+                    Console.Write(root.data + " , ");
+                    
+                    if (root.left != null)
+                        S2.Push(root.left);
+                    if (root.right != null)
+                        S2.Push(root.right);
+
+                }
+
+                Console.WriteLine();
+
+                while (S2.Count != 0)
+                {
+                    root = S2.Pop();
+                    Console.Write(root.data + " , ");
+
+                    
+                    if (root.right != null)
+                        S1.Push(root.right);
+                    if (root.left != null)
+                        S1.Push(root.left);
+
+                }
+            }
+
+            
+
+        }
+    }
+}
